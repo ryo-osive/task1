6aae87ee88ab36bebf77d1f69b98e42e31a22fbc
Ryo Osive <ryo.osive@gmail.com>
2020-04-23 07:40:48 +0530

    master

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Task 1"/>
   </div>
 </template>
 
@@ -25,4 +25,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+@import url(https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css);
+
 </style>
+
